c5ab3201dc377590826e428ac87dd129489949e0
Aditya Panicker <adipanicker7@gmail.com>
2025-10-14 13:14:45 +0530

    added more endpoints

--- routes/users.js
+++ routes/users.js
@@ -36,6 +36,16 @@ router.get("/:email", (req, res) => {
   res.send(filtered_users);
 });
 
+//lastName
+router.get("/lastName/:lastName", (req, res) => {
+  // Extract the lastName parameter from the request URL
+  const lastName = req.params.lastName;
+  // Filter the users array to find users whose lastName matches the extracted lastName parameter
+  let filtered_lastname = users.filter((user) => user.lastName === lastName);
+  // Send the filtered_lastname array as the response to the client
+  res.send(filtered_lastname);
+});
+
 // POST request: Create a new user
 router.post("/", (req, res) => {
   users.push({
